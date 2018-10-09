43e0d025bf5ad16dc0a970698d69d3496d34b17e
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-10-09 23:27:04 +0200

    models

--- kavarna/models.py
+++ kavarna/models.py
@@ -1,53 +1,66 @@
 from django.db import models
 
-class MyModel(models.Model):
-    """ Parent for all the models. """
-    name = models.CharField(max_length=64)
-class MyAddress(models.Model):
-    """ Address. """
-    street = models.CharField(max_length=64)
-    housenumber = models.PositiveIntegerField(blank=True)
-    city = models.CharField(max_length=64)
-class MySchedule(models.Model):
-    """ Schedule. """
-    opensAt = models.TimeField()
-    closesAt = models.TimeField()
-
-
-class CoffeePreparation(MyModel):
+class CoffeePreparation(models.Model):
     """ Preparation of coffee. """
+    name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
 
-class CoffeeBean(MyModel):
+class CoffeeBean(models.Model):
     """ Coffee bean type. """
+    name = models.CharField(max_length=64)
     origin = models.CharField(max_length=64, blank=True)
     aroma = models.CharField(max_length=64, blank=True)
     acidity = models.PositiveSmallIntegerField(blank=True)
 
-class Coffee(MyModel):
+class Coffee(models.Model):
     """ Coffee served in some cafe. """
+    name = models.CharField(max_length=64)
     preparation = CoffeePreparation()
-    beans = models.ManyToManyField(CoffeeBean, blank=True)
+    beans = models.ManyToManyField(CoffeeBean, blank=True)  
 
-class User(MyModel):
+class User(models.Model):
     """ User of the system. """
+    name = models.CharField(max_length=64)
     email = models.EmailField()
     surname = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
+    preparation = models.ManyToManyField(CoffeePreparation)
 
-class Cafe(MyModel):
+class Cafe(models.Model):
     """ Cafe. """
-    address = MyAddress()
-    schedule = MySchedule()
+    name = models.CharField(max_length=64)
+    street = models.CharField(max_length=64)
+    housenumber = models.PositiveIntegerField(blank=True)
+    city = models.CharField(max_length=64)
+    opensAt = models.TimeField()
+    closesAt = models.TimeField()
     capacity = models.PositiveSmallIntegerField(blank=True)
     description = models.TextField(blank=True)
-    owner = User()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    #employees = models.ManyToManyField(User)
 
-class Event(MyModel):
+class Event(models.Model):
     """ Event. """
+    name = models.CharField(max_length=64)
     price = models.IntegerField()
     capacity = models.IntegerField()
     participants = models.ManyToManyField(User)
+    place = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+
+class Evaluation(models.Model):
+    """ Evaluation. """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField()
+
+class Reaction(models.Model):
+    """ Comment. """
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    comment = models.ForeignKey('self', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+    date = models.DateTimeField()
+    evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
 
 
 
