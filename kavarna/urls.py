83591c9e79d8221dd27b4942114ef7be2f7a026c
qondra <qondra@localhost.localdomain>
2018-10-25 20:38:49 +0200

    profil + kavarny

--- kavarna/urls.py
+++ kavarna/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('cafes/', views.cafes, name='cafes'),
     path('profile/', views.profile, name='profile'),
     path('profile_cafe/', views.profile_cafe, name='profile_cafe'),
+    path('deletecafe/', views.deletecafe, name='deletecafe'),
     path('addcafe/', views.addcafe, name='addcafe'),
     path('logout/', views.logout, name='logout'),
     path('cafe/', views.cafe, name='cafe')
