93a6280873bc503e2bd5ccf94568daf8ab51c7b0
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-12-01 23:45:00 +0100

    wsgi

--- kavarna/wsgi.py
+++ kavarna/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kavarna.settings")
 
